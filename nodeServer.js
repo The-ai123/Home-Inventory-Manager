302d48cf88d1d90038a0ed5dc06ade3717e8ff4c
The-ai123 <sans34678@gmail.com>
2024-01-16 20:27:44 -0500

    Added basic test for nodeServer.js

--- nodeServer.js
+++ nodeServer.js
@@ -1,6 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    switch(req.url){
+        case "index":
+            fs.readFile('index.html', function (err, data) {
+                res.write(data);
+                return res.end();
+            });
+        default:
+            fs.readFile('index.html', function (err, data) {
+
+                res.write(data);
+                return res.end();
+            });
+    }
+
+    //default
+
+
 }).listen(8080);
